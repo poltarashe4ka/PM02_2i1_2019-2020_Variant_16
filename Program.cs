6ebdeaf7136cfd664166577e589505ec76c6a4fe
poltarashe4ka <56540658+poltarashe4ka@users.noreply.github.com>
2020-06-19 15:49:04 +0400

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Inversia
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите строку");
+            string Numbers = Convert.ToString(Console.ReadLine());
+            Console.WriteLine("Введите числа которые хотите выбрать");
+            int a = Convert.ToInt32(Console.ReadLine());
+            for (int i = 0; i < Numbers.Length; i++)
+            {
+                if (a == i)
+                {
+                    Console.WriteLine($"Выбранные числа {i}");
+                }
+            }
+            Console.ReadLine();
+        }
+    }
+}
